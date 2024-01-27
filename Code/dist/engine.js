301d60471bde8c0d1b64a2d39584cc940a0c81aa
Ex Machinis <david.rozak@darwinriver.com>
2024-01-27 14:36:49 -0500

    Set isMoving to false when complete.

--- Code/dist/engine.js
+++ Code/dist/engine.js
@@ -1,4 +1,8 @@
 "use strict";
+/**
+ * This is the game engine that is used to log, manage, and resolve in-game actions
+ * that are submitted by the robot and NPCs.
+ */
 class Engine {
     #world;
     #actions;
@@ -22,6 +26,11 @@ class Engine {
         tile: "x",
         speed: 1.0 // Meters per second
     };
+    /**
+     * Creates a game engine object that is based on the specified world parameters.
+     *
+     * @param world The set of parameters that define the game world.
+     */
     constructor(world) {
         this.#world = world;
         this.#dungeon = new Dungeon(world);
@@ -30,33 +39,62 @@ class Engine {
         this.#status.targ = this.#dungeon.enter;
         this.#status.tile = this.#dungeon.getTile(this.#status.pos);
     }
+    /**
+     * Instructs the game engine to process game events for a number of seconds while the
+     * robot waits to request any further actions.
+     *
+     * @param time The time in seconds that the robot has asked to wait.
+     */
     wait(time) {
         for (let i = 0; i < time; i++) {
             this.#status.time++;
-            this.evaluate();
+            this.#evaluate();
         }
     }
-    addAction(type, params, delay) {
-        this.#actions.push(new Action(type, params, this.#status.time + delay));
+    /**
+     * Adds an action to the action cue and assigns it a completion time.
+     *
+     * @param type This is the type of action that will be performed.
+     * @param params These are the action-specific parameters that are needed
+     * to describe the action.
+     * @param completed This is the time in seconds that the action will be completed.
+     */
+    addAction(type, params, completed) {
+        this.#actions.push(new Action(type, params, this.#status.time + completed));
     }
-    move(dir) {
-        this.#status.targ = this.#status.pos.add(this.#direction[dir]);
+    /**
+     * Evaluates the move action.
+     *
+     * @param direction A number that represents that diraction that the robot will
+     * move with zero being north and 1 - 7 representing the additional compass points
+     * in a clockwise direction.
+     */
+    #move(direction) {
+        this.#status.targ = this.#status.pos.add(this.#direction[direction]);
         if (this.#dungeon.getTileSpeed(this.#status.targ) > 0) {
             this.#status.pos = this.#status.targ;
         }
+        this.#status.isMoving = false;
     }
-    evaluate() {
+    /**
+     * Evaluates any actions in the action buffer that need to be performed and evaluates
+     * them in the order that they are scheduled to be completed.
+     */
+    #evaluate() {
         if (this.#actions.length > 0) {
             this.#actions.sort((a, b) => a.time - b.time);
             if (this.#actions[0].time <= this.#status.time) {
                 switch (this.#actions[0].type) {
                     case "move":
-                        this.move(this.#actions[0].params);
+                        this.#move(this.#actions[0].params);
                 }
                 this.#actions.shift();
             }
         }
     }
+    /**
+     * Returns the robot's current status.
+     */
     get status() {
         return this.#status;
     }
