b16ba40ae82db8b5c25b678d2faf20dca4db4dfb
Ex Machinis <david.rozak@darwinriver.com>
2024-02-08 05:04:12 -0500

    Moved power and max power to battery class.

--- Code/dist/status.js
+++ Code/dist/status.js
@@ -5,7 +5,6 @@ class Status {
     pos;
     scan;
     model;
-    // currentPower: number;
     currentHps;
     // Equipped eqipment
     core;
@@ -16,7 +15,6 @@ class Status {
         this.name = name;
         this.scan = new Scan(world.size);
         this.model = this.world.model[0];
-        // this.currentPower = this.model.maxPower;
         this.currentHps = this.model.maxHps;
         this.pos = world.entrances[robotID];
         // Equip model's equipment
